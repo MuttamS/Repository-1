23be9e297fd2458c9ffecb0003363661514e036a
Ravi <muttamsettyravichandrudu@gmail.com>
2025-09-23 01:02:11 +0530

    some message

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/
+
+Person1=Keshava
+Person2=Ravi
+
+echo "Ravi:: Hi Keshava, How are you?."
+echo "Keshava:: Hi Ravi, I am fine , how about you?."
+echo "Ravi:: I am fine Keshava, what are you doing?."
+echo "Keshava:: Nothing Ravi i just sit in room."
