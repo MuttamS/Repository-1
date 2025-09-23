c836c0111eb11e372ba9aecc5b67135132f2690f
Ravi <muttamsettyravichandrudu@gmail.com>
2025-09-23 18:52:52 +0530

    Passed the arguments in variables

---
+++ varibles-arg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash/
+
+# Arguments
+
+Person=$1
+Person=$2
+
+echo "$1:: Hi putin, wellcome to UASA."
+echo "$2:: Hi Trump, Thank you"
+echo "$1:: I have a deal with you."
+echo "$2:: Yes, If you join ti Ukevni in Rusia."
+echo "$1:: Okay, Putin"
+echo "$2:: Thank you, We are stoping the war."
