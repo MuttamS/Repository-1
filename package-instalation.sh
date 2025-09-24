d460030b4f4285dec09902c7e3a4df1f08b9e25f
Ravi <muttamsettyravichandrudu@gmail.com>
2025-09-24 12:06:13 +0000

    mysql install

--- package-instalation.sh
+++ package-instalation.sh
@@ -1,15 +1,15 @@
 #!/bin/bash/
 
-USERID=$(ID -U)
+USERID=$(id -u)
 
-if[ $USERID -ne 0 ]; then
+if [ $USERID -ne 0 ]; then
 
     echo "ERROR:: please run this script with root privelages"
 fi
 
 dnf install mysql -y
 
-if[ S? -ne 0 ]; then 
+if [ S? -ne 0 ]; then 
 
     echo "ERROR:: Installing mysql is failure"
 
